7e9d07bf3ef92baaa735d6bb0d92507409704ff2
inqu <inqu16@gmail.com>
2021-08-17 11:00:00 +0000

    Feat: express test

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log("hello world!!");
+import * as express from "express";
+
+const app: express.Express = express();
+
+const port: number = 8000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req);
+
+  res.send("hello World!");
+});
+
+app.post("/", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send("success");
+});
+
+app.listen(port, () => {
+  console.log("dsffdssdf");
+});
