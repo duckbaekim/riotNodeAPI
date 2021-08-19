3f3ffe79caa84809311d8c87c0624853c7249d67
inqu <inqu16@gmail.com>
2021-08-19 21:58:50 +0900

    Feat: CRUD Test CR완료

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,70 @@ app.get("/", (req, res, next) => {
   console.log("특정 미들웨어 사용");
   next();
 });
+// 미들웨어 끗
+//* Read 고양이 전체 조회
+app.get("/cats", (req: express.Request, res: express.Response) => {
+  try {
+    const cats = Cat;
+    // throw new Error("db connect Error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (e) {
+    res.status(400).send({
+      success: false,
+      error: e.message,
+    });
+  }
+  res.send({ cats: Cat });
+});
 
-app.get("/", (req: express.Request, res: express.Response) => {
+//* Read 특정 고양이 조회
+app.get("/cats/:id", (req: express.Request, res: express.Response) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const cat = Cat.find((cat) => cat.id === id);
+
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (e) {
+    res.status(400).send({
+      success: false,
+      error: e.message,
+    });
+  }
+  res.send({ cats: Cat });
+});
+//* json middle ware
+app.use(express.json());
+
+//* CREATE 새로운 고양이 추가
+app.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    res.status(200).send({
+      success: true,
+      data: {},
+    });
+  } catch (e) {
+    res.status(400).send({
+      success: false,
+      error: e.message,
+    });
+  }
+});
+
+app.get("/cats", (req: express.Request, res: express.Response) => {
   // console.log(req.rawHeaders[1]);
   res.send({ cats: Cat });
 });
